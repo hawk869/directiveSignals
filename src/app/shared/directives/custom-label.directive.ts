cf8e1cabe98d22b5842b531bfc10c25c86885eac
Daniel Esteban <decepeda@misena.edu.co>
2024-02-29 19:16:55 -0500

    Se cambia el mensaje de la etiqueta

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]'
@@ -7,10 +8,17 @@ export class CustomLabelDirective implements OnInit {
 
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color( value: string ) {
     this._color = value;
     this.setStyle();
+  }
+
+  @Input() set errors( value: ValidationErrors | null | undefined ) {
+    this._errors = value;
+    console.log(value);
+    this.setErrorMessage();
   }
 
   constructor( private el: ElementRef<HTMLElement> ) {
@@ -19,11 +27,36 @@ export class CustomLabelDirective implements OnInit {
     this.htmlElement = el;
    }
   ngOnInit(): void {
-    console.log('Directiva - NgOnInit');
+    // console.log('Directiva - NgOnInit');
+    this.setStyle();
   }
 
   setStyle(): void {
     if ( !this.htmlElement ) return;
     this.htmlElement!.nativeElement.style.color = this._color
+  }
+
+  setErrorMessage(): void {
+    if ( !this.htmlElement ) return;
+    if ( !this._errors ) {
+      this.htmlElement.nativeElement.innerText = '';
+      return;
+    }
+    const errors = Object.keys(this._errors);
+    
+    if ( errors.includes('required')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo es requerido';
+      return;
+    }
+    if ( errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+      this.htmlElement.nativeElement.innerText = `Mínimo ${ current }/${ min } caracteres.`;
+      return;
+    }
+    if ( errors.includes('email')) {
+      this.htmlElement.nativeElement.innerText = 'Este campo debe ser un correo';
+      return;
+    }
   }
 }
